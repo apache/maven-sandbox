42fd0d55efec840549c038a96ef6906c3114e274
John Dennis Casey <jdcasey@apache.org>
2007-03-14 15:31:16 +0000

    Plugin that uses the refactored lifecycle (in the 2.1-lifecycle-refactor branch) to output a build plan.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@518183 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/maven-lifecycle-plugin/src/main/java/org/apache/maven/plugin/lifecycle/BuildPlanMojo.java
@@ -0,0 +1,148 @@
+package org.apache.maven.plugin.lifecycle;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.lifecycle.LifecycleLoaderException;
+import org.apache.maven.lifecycle.LifecycleSpecificationException;
+import org.apache.maven.lifecycle.plan.BuildPlan;
+import org.apache.maven.lifecycle.plan.BuildPlanUtils;
+import org.apache.maven.lifecycle.plan.BuildPlanner;
+import org.apache.maven.lifecycle.plan.LifecyclePlannerException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.IOUtil;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Retrieves the build plan for the current project, and displays it to the logger's INFO log-level,
+ * or a file.
+ *
+ * @goal build-plan
+ */
+public class BuildPlanMojo
+    extends AbstractMojo
+{
+    /**
+     * File for writing the build-plan.
+     * 
+     * @parameter
+     */
+    private File output;
+    
+    /**
+     * Comma-separated list of tasks to complete in the proposed build.
+     * 
+     * @parameter expression="${tasks}" default-value="${package}"
+     * @required
+     */
+    private String tasks;
+    
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+    
+    /**
+     * @component
+     */
+    private BuildPlanner buildPlanner;
+    
+    public void execute()
+        throws MojoExecutionException
+    {
+        StringTokenizer tokens = new StringTokenizer( tasks, "," );
+        List taskList = new ArrayList( tokens.countTokens() );
+        
+        while( tokens.hasMoreTokens() )
+        {
+            taskList.add( tokens.nextToken().trim() );
+        }
+        
+        BuildPlan buildPlan;
+        try
+        {
+            buildPlan = buildPlanner.constructLifecyclePlan( taskList, project );
+        }
+        catch ( LifecycleLoaderException e )
+        {
+            throw new MojoExecutionException( "Failed to construct build plan. Reason: " + e.getMessage(), e );
+        }
+        catch ( LifecycleSpecificationException e )
+        {
+            throw new MojoExecutionException( "Failed to construct build plan. Reason: " + e.getMessage(), e );
+        }
+        catch ( LifecyclePlannerException e )
+        {
+            throw new MojoExecutionException( "Failed to construct build plan. Reason: " + e.getMessage(), e );
+        }
+        
+        String listing;
+        try
+        {
+            listing = BuildPlanUtils.listBuildPlan( buildPlan );
+        }
+        catch ( LifecycleSpecificationException e )
+        {
+            throw new MojoExecutionException( "Failed to list build plan. Reason: " + e.getMessage(), e );
+        }
+        catch ( LifecyclePlannerException e )
+        {
+            throw new MojoExecutionException( "Failed to list build plan. Reason: " + e.getMessage(), e );
+        }
+        
+        if ( output != null )
+        {
+            if ( output.getParentFile() != null )
+            {
+                output.getParentFile().mkdirs();
+            }
+            
+            FileWriter writer = null;
+            try
+            {
+                writer = new FileWriter( output );
+                
+                writer.write( "Project: " + project.getId() );
+                writer.write( "\nTasks: " + tasks );
+                writer.write( "\nBuildPlan:\n" );
+                writer.write( listing );
+                writer.flush();
+            }
+            catch ( IOException e )
+            {
+                throw new MojoExecutionException( "Failed to write build plan to: " + output + ". Reason: " + e.getMessage(), e );
+            }
+            finally
+            {
+                IOUtil.close( writer );
+            }
+        }
+        else
+        {
+            getLog().info( "\n\nProject: " + project.getId() + "\nTasks: " + tasks + "\nBuild Plan:\n" + listing + "\n\n" );
+        }
+    }
+}
