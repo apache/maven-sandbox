c0c2c52febafc579b3ebba861e1a623594bf4af7
Vincent Siveton <vsiveton@apache.org>
2007-06-06 22:42:37 +0000

    JXR-54: Discuss about Forrestdoc and JXR
    o Play with Forrestdoc
    o created a Maven project for Forrestdoc
    o created a Maven Forrestdoc plugin, which generates a Forrestdoc report
    o added doc and test cases
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@544978 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jxr/forrestdoc/maven-forrestdoc-plugin/src/main/java/org/apache/maven/plugin/forrestdoc/AbstractJavasrcMojo.java
@@ -0,0 +1,134 @@
+package org.apache.maven.plugin.forrestdoc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.forrest.forrestdoc.java.src.JavaSrcTask;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+
+/**
+ * Base class which wraps all <code>JavaSrcTask</code> functionalities.
+ *
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ */
+public abstract class AbstractJavasrcMojo
+    extends AbstractMojo
+{
+    // ----------------------------------------------------------------------
+    // Mojo parameters
+    // ----------------------------------------------------------------------
+
+    /**
+     * The Maven Project Object
+     *
+     * @parameter expression="${project}"
+     * @required
+     */
+    protected MavenProject project;
+
+    // ----------------------------------------------------------------------
+    // JavaSrcTask parameters
+    // ----------------------------------------------------------------------
+
+    /**
+     * The source directory to be scanned.
+     *
+     * @parameter expression="${project.build.sourceDirectory}"
+     * @required
+     * @readonly
+     * @see org.apache.forrest.forrestdoc.java.src.JavaSrcTask#srcDir
+     */
+    protected File srcDir;
+
+    /**
+     * The output directory.
+     *
+     * @parameter expression="${project.build.directory}/javasrc"
+     * @required
+     * @readonly
+     * @see org.apache.forrest.forrestdoc.java.src.JavaSrcTask#destDir
+     */
+    protected File outputDirectory;
+
+    /**
+     * True to apply a recursive scan.
+     *
+     * @parameter expression="${recurse}" default-value="true"
+     * @see org.apache.forrest.forrestdoc.java.src.JavaSrcTask#recurse
+     */
+    private boolean recurse;
+
+    /**
+     * The title of the generated HTML report.
+     *
+     * @parameter expression="${title}" default-value="${project.name} ${project.version} Reference"
+     * @see org.apache.forrest.forrestdoc.java.src.JavaSrcTask#title
+     */
+    private String windowTitle;
+
+    /**
+     * True to verbose the scan.
+     *
+     * @parameter expression="${verbose}" default-value="false"
+     * @see org.apache.forrest.forrestdoc.java.src.JavaSrcTask#verbose
+     */
+    private boolean verbose;
+
+    /**
+     * Execute the <code>JavaSrcTask</code>
+     *
+     * @throws MojoExecutionException if any
+     * @see org.apache.forrest.forrestdoc.java.src.JavaSrcTask#execute()
+     */
+    public void executeJavaSrcTask()
+        throws MojoExecutionException
+    {
+        // TODO need to add a custom footer
+        // @see org.apache.forrest.forrestdoc.java.src.Pass2#createPackageSummaryFiles()
+        // @see org.apache.forrest.forrestdoc.java.src.Pass2#createOverviewSummaryFrame()
+
+        JavaSrcTask task = new JavaSrcTask();
+
+        Project antProject = new Project();
+        antProject.setBaseDir( project.getBasedir() );
+        task.setProject( antProject );
+
+        task.setDestDir( outputDirectory );
+        task.setSrcDir( srcDir );
+        task.setTitle( windowTitle );
+        task.setVerbose( verbose );
+        task.setRecurse( recurse );
+
+        try
+        {
+            task.execute();
+        }
+        catch ( BuildException e )
+        {
+            throw new MojoExecutionException( "BuildException: " + e.getMessage() );
+        }
+    }
+}
---
+++ jxr/forrestdoc/maven-forrestdoc-plugin/src/main/java/org/apache/maven/plugin/forrestdoc/JavasrcReport.java
@@ -0,0 +1,209 @@
+package org.apache.maven.plugin.forrestdoc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Locale;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.reporting.MavenReport;
+import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.doxia.sink.Sink;
+import org.codehaus.plexus.i18n.I18N;
+import org.codehaus.plexus.util.StringUtils;
+
+/**
+ * Creates an html-based, cross referenced version of Java source code
+ * based on Forrestdoc project.
+ *
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ * @goal javasrc
+ */
+public class JavasrcReport
+    extends AbstractJavasrcMojo
+    implements MavenReport
+{
+    // ----------------------------------------------------------------------
+    // Report Parameters
+    // ----------------------------------------------------------------------
+
+    /**
+     * Internationalization.
+     *
+     * @component
+     */
+    protected I18N i18n;
+
+    /**
+     * Specifies the destination directory where javasrc generates HTML files.
+     *
+     * @parameter expression="${project.reporting.outputDirectory}/javasrc"
+     * @required
+     */
+    private File reportOutputDirectory;
+
+    /**
+     * The name of the Antlr report.
+     *
+     * @parameter expression="${name}" default-value="Forrestdoc"
+     */
+    private String name;
+
+    /**
+     * The description of the Antlr report.
+     *
+     * @parameter expression="${description}" default-value="Source code/Documentation management system."
+     */
+    private String description;
+
+    /**
+     * @see org.apache.maven.plugin.AbstractMojo#execute()
+     */
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        super.executeJavaSrcTask();
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#generate(org.codehaus.doxia.sink.Sink, java.util.Locale)
+     */
+    public void generate( Sink sink, Locale locale )
+        throws MavenReportException
+    {
+        outputDirectory = reportOutputDirectory;
+
+        try
+        {
+            execute();
+        }
+        catch ( MojoExecutionException e )
+        {
+            throw new MavenReportException( "An error has occurred in " + getName( Locale.ENGLISH )
+                + " report generation.", e );
+        }
+        catch ( MojoFailureException e )
+        {
+            throw new MavenReportException( "An error has occurred in " + getName( Locale.ENGLISH )
+                + " report generation.", e );
+        }
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#canGenerateReport()
+     */
+    public boolean canGenerateReport()
+    {
+        // TODO aggregate report
+        boolean canGenerate = true;
+        for (Iterator it = project.getCompileSourceRoots().iterator(); it.hasNext();)
+        {
+            canGenerate = canGenerate && new File( (String) it.next() ).exists();
+        }
+
+        if ( !project.isExecutionRoot() )
+        {
+            canGenerate = false;
+        }
+
+        return canGenerate;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getCategoryName()
+     */
+    public String getCategoryName()
+    {
+        return CATEGORY_PROJECT_REPORTS;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getOutputName()
+     */
+    public String getOutputName()
+    {
+        return "javasrc/index";
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getName(java.util.Locale)
+     */
+    public String getName( Locale locale )
+    {
+        if ( StringUtils.isEmpty( name ) )
+        {
+            return i18n.getString( "forrestdoc-report", locale, "report.name" );
+        }
+
+        return name;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getDescription(java.util.Locale)
+     */
+    public String getDescription( Locale locale )
+    {
+        if ( StringUtils.isEmpty( description ) )
+        {
+            return i18n.getString( "forrestdoc-report", locale, "report.description" );
+        }
+
+        return description;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getReportOutputDirectory()
+     */
+    public File getReportOutputDirectory()
+    {
+        if ( reportOutputDirectory == null )
+        {
+            return outputDirectory;
+        }
+
+        return reportOutputDirectory;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#setReportOutputDirectory(java.io.File)
+     */
+    public void setReportOutputDirectory( File reportOutputDirectory )
+    {
+        if ( ( reportOutputDirectory != null ) && ( !reportOutputDirectory.getAbsolutePath().endsWith( "javasrc" ) ) )
+        {
+            this.reportOutputDirectory = new File( reportOutputDirectory, "javasrc" );
+        }
+        else
+        {
+            this.reportOutputDirectory = reportOutputDirectory;
+        }
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#isExternalReport()
+     */
+    public boolean isExternalReport()
+    {
+        return true;
+    }
+}
