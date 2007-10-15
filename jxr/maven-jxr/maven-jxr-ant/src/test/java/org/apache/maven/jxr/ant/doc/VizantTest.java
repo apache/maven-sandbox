354e24bdd290cf8b07d0d1fcc92785dc3c9ccd46
Vincent Siveton <vsiveton@apache.org>
2007-10-15 12:23:42 +0000

    o added Vizant test case
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@584768 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jxr/maven-jxr/maven-jxr-ant/src/test/java/org/apache/maven/jxr/ant/doc/VizantTest.java
@@ -0,0 +1,61 @@
+package org.apache.maven.jxr.ant.doc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.apache.tools.ant.Project;
+
+/**
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ */
+public class VizantTest
+    extends TestCase
+{
+    /**
+     * Call Vizabt task
+     *
+     * @throws Exception if any.
+     */
+    public void testExecute()
+        throws Exception
+    {
+        final String basedir = new File( "" ).getAbsolutePath();
+
+        Project antProject = new Project();
+        antProject.setBasedir( basedir );
+
+        File targetDir = new File( basedir, "target/unit/vizant" );
+
+        File build = new File( basedir, "src/test/resources/build.xml" );
+        File buildGraph = new File( targetDir, "buildgraph.xml" );
+
+        Vizant task = new Vizant();
+        task.setProject( antProject );
+        task.init();
+        task.setAntfile( build );
+        task.setOutfile( buildGraph );
+        task.setUniqueref( true );
+        task.execute();
+    }
+}
