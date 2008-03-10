71ab1e59b604b5c142ce82c2d56b2d9287921da5
Dennis Lundberg <dennisl@apache.org>
2008-03-10 21:24:48 +0000

    o Just create the report - nothing else.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@635697 13f79535-47bb-0310-9956-ffa450edef68

--- shared/reports/maven-plugins.sh
+++ shared/reports/maven-plugins.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-export JAVA_HOME=/opt/java/sdk/current                                                                                                                          
-export JAVA_CMD=$JAVA_HOME/bin/java                                                                                                                             
-export JAVA_OPTS="-ms32m -mx256m"                                                                                                                               
-export JAVA="$JAVA_CMD $JAVA_OPTS"           
+export JAVA_HOME=/opt/java/sdk/current
+export JAVA_CMD=$JAVA_HOME/bin/java
+export JAVA_OPTS="-ms32m -mx256m"
+export JAVA="$JAVA_CMD $JAVA_OPTS"
 
-(
-
-$JAVA -jar swizzle-jirareport-1.2.1-SNAPSHOT-dep.jar maven-plugins.vm
-
-) 2>&1 | tee $HOME/repository-staging/to-ibiblio/reports/plugins/plugin-issues.txt
+$JAVA -jar swizzle-jirareport-1.2.1-SNAPSHOT-dep.jar maven-plugins.vm > maven-plugins.txt
