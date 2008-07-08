8c13337d78039d2da5077090b946b5c68d3ea7ac
Britton Isbell <sisbell@apache.org>
2008-07-08 19:21:59 +0000

    Builder code.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/branches/SI_MAVEN_2_1@674921 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-project/src/main/java/org/apache/maven/project/builder/IdModelContainerFactory.java
@@ -0,0 +1,101 @@
+package org.apache.maven.project.builder;
+
+import org.apache.maven.shared.model.*;
+
+import java.util.*;
+
+public class IdModelContainerFactory implements ModelContainerFactory {
+
+    private static final Collection<String> uris = Collections.unmodifiableList(Arrays.asList(
+            //ProjectUri.Build.Plugins.Plugin.Executions.Execution.xUri,
+            //ProjectUri.Build.PluginManagement.Plugins.Plugin.Executions.Execution.xUri,
+            ProjectUri.PluginRepositories.PluginRepository.xUri,
+            ProjectUri.Repositories.Repository.xUri,
+            ProjectUri.Reporting.Plugins.Plugin.ReportSets.ReportSet.xUri,
+            ProjectUri.Profiles.Profile.xUri
+    ));
+
+    public Collection<String> getUris() {
+        return uris;
+    }
+
+    public ModelContainer create(List<ModelProperty> modelProperties) {
+        if (modelProperties == null || modelProperties.size() == 0) {
+            throw new IllegalArgumentException("modelProperties: null or empty");
+        }
+        return new IdModelContainer(modelProperties);
+    }
+
+    private static class IdModelContainer implements ModelContainer {
+
+        private String id;
+
+        private List<ModelProperty> properties;
+
+        private IdModelContainer(List<ModelProperty> properties) {
+            this.properties = new ArrayList<ModelProperty>(properties);
+            this.properties = Collections.unmodifiableList(this.properties);
+
+            for (ModelProperty mp : properties) {
+                if (mp.getUri().endsWith("/id")) {
+                    this.id = mp.getValue();
+                }
+            }
+
+            //   if (id == null) {
+            //       throw new IllegalArgumentException("properties does not contain id");
+            //   }
+        }
+
+        public ModelContainerAction containerAction(ModelContainer modelContainer) {
+            if (modelContainer == null) {
+                throw new IllegalArgumentException("modelContainer: null");
+            }
+
+            if (!(modelContainer instanceof IdModelContainer)) {
+                throw new IllegalArgumentException("modelContainer: wrong type");
+            }
+
+            IdModelContainer c = (IdModelContainer) modelContainer;
+            if (c.id == null || id == null) {
+                return ModelContainerAction.NOP;
+            }
+            return (c.id.equals(id)) ? ModelContainerAction.JOIN : ModelContainerAction.NOP;
+        }
+
+        public ModelContainer createNewInstance(List<ModelProperty> modelProperties) {
+            return new IdModelContainer(modelProperties);
+        }
+
+        public void sort(List<ModelProperty> modelProperties) {
+            //Collections.sort(modelProperties, new IdModelComparator());
+         /*   System.out.println("END SORT");
+            for(ModelProperty mp : modelProperties) {
+                System.out.println(mp);
+            }
+            */
+        }
+
+        public List<ModelProperty> getProperties() {
+            return properties;
+        }
+
+        public String toString() {
+            return "ID = " + id;
+        }
+    }
+
+    private static class IdModelComparator implements Comparator {
+        public int compare(Object o1, Object o2) {
+            ModelProperty a = (ModelProperty) o1;
+            ModelProperty b = (ModelProperty) o2;
+            System.out.println(a + " : " + b);
+            if (a.isParentOf(b)) {
+                System.out.println("IS PARENT ABOVE:");
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ maven-project/src/main/java/org/apache/maven/project/builder/PomClassicTransformer.java
@@ -0,0 +1,180 @@
+package org.apache.maven.project.builder;
+
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import org.apache.maven.shared.model.*;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.*;
+
+/**
+ * Provides methods for transforming model properties into a domain model for the pom classic format and vice versa.
+ */
+public final class PomClassicTransformer implements ModelTransformer {
+
+    private Set<String> uris;
+
+    public PomClassicTransformer() {
+
+    }
+
+    public String getBaseUri() {
+        return ProjectUri.baseUri;
+    }
+
+    public PomClassicTransformer(Set<String> uris) {
+        this.uris = new HashSet<String>(Arrays.asList(
+                "http://apache.org/maven/project/build/resources#collection",
+                "http://apache.org/maven/project/build/plugins/plugin/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/profiles/profile/build/pluginManagement/plugins#collection",
+                "http://apache.org/maven/project/profiles/profile/build/plugins/plugin/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/profiles/profile/reporting/plugins#collection",
+                "http://apache.org/maven/project/dependencyManagement/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/profiles/profile/build/testResources#collection",
+                "http://apache.org/maven/project/reporting/plugins/plugin/reportSets#collection",
+                "http://apache.org/maven/project/pluginRepositories#collection",
+                "http://apache.org/maven/project/profiles/profile/build/pluginManagement/plugins/plugin/dependencies#collection",
+                "http://apache.org/maven/project/profiles/profile/build/resources#collection",
+                "http://apache.org/maven/project/profiles/profile/build/pluginManagement/plugins/plugin/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/licenses#collection",
+                "http://apache.org/maven/project/build/plugins/plugin/dependencies#collection",
+                "http://apache.org/maven/project/profiles/profile/dependencyManagement/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/profiles/profile/build/plugins/plugin/dependencies#collection",
+                "http://apache.org/maven/project/build/testResources#collection",
+                "http://apache.org/maven/project/profiles/profile/pluginRepositories#collection",
+                "http://apache.org/maven/project/build/pluginManagement/plugins#collection",
+                "http://apache.org/maven/project/profiles#collection",
+                "http://apache.org/maven/project/reporting/plugins#collection",
+                "http://apache.org/maven/project/build/pluginManagement/plugins/plugin/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/build/pluginManagement/plugins/plugin/executions#collection",
+                "http://apache.org/maven/project/profiles/profile/dependencies/dependency/exclusions#collection",
+                "http://apache.org/maven/project/dependencies#collection",
+                "http://apache.org/maven/project/contributors#collection",
+                "http://apache.org/maven/project/developers#collection",
+                "http://apache.org/maven/project/build/plugins#collection",
+                "http://apache.org/maven/project/profiles/profile/build/pluginManagement/plugins/plugin/executions#collection",
+                "http://apache.org/maven/project/profiles/profile/dependencies#collection",
+                "http://apache.org/maven/project/mailingLists#collection",
+                "http://apache.org/maven/project/profiles/profile/dependencyManagement/dependencies#collection",
+                "http://apache.org/maven/project/profiles/profile/repositories#collection",
+                "http://apache.org/maven/project/build/extensions#collection",
+                "http://apache.org/maven/project/build/plugins/plugin/executions#collection",
+                "http://apache.org/maven/project/repositories#collection",
+                "http://apache.org/maven/project/ciManagement/notifiers#collection",
+                "http://apache.org/maven/project/dependencyManagement/dependencies#collection",
+                "http://apache.org/maven/project/build/pluginManagement/plugins/plugin/dependencies#collection",
+                "http://apache.org/maven/project/profiles/profile/reporting/plugins/plugin/reportSets#collection",
+                "http://apache.org/maven/project/profiles/profile/build/plugins#collection",
+                "http://apache.org/maven/project/profiles/profile/build/plugins/plugin/executions#collection",
+                "http://apache.org/maven/project/modules#collection"
+        ));
+    }
+
+    public DomainModel transformToDomainModel(List<ModelProperty> properties) throws IOException {
+        if (properties == null) {
+            throw new IllegalArgumentException("properties: null");
+        }
+        String xml = null;
+        try {
+            xml = ModelMarshaller.unmarshalModelPropertiesToXml(properties, ProjectUri.baseUri);
+            return new PomClassicDomainModel(new MavenXpp3Reader().read(new StringReader(xml)));
+        } catch (XmlPullParserException e) {
+            /*
+            StringBuffer sb = new StringBuffer("\r\n");
+            for (ModelProperty mp : properties) {
+                sb.append(mp).append("\r\n");
+            }
+            */
+            throw new IOException(e + ":\r\n" + xml);
+        }
+    }
+
+    public List<ModelProperty> transformToModelProperties(List<DomainModel> domainModels) throws IOException {
+        if (domainModels == null || domainModels.isEmpty()) {
+            throw new IllegalArgumentException("domainModels: null or empty");
+        }
+
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        List<String> projectNames = new ArrayList<String>();
+        StringBuffer scmUrl = new StringBuffer();
+
+        for (DomainModel domainModel : domainModels) {
+            if (!(domainModel instanceof PomClassicDomainModel)) {
+                throw new IllegalArgumentException("domainModels: Invalid domain model");
+            }
+
+            List<ModelProperty> tmp = ModelMarshaller.marshallXmlToModelProperties(
+                    ((PomClassicDomainModel) domainModel).getInputStream(), ProjectUri.baseUri, uris);
+
+            //Missing Version Rule
+            if (getPropertyFor(ProjectUri.version, tmp) == null) {
+                ModelProperty parentVersion = getPropertyFor(ProjectUri.Parent.version, tmp);
+                tmp.add(new ModelProperty(ProjectUri.version, parentVersion.getValue()));
+            }
+
+            //Modules Not Inherited Rule
+            if (domainModels.indexOf(domainModel) != 0) {
+                ModelProperty modulesProperty = getPropertyFor(ProjectUri.Modules.xUri, tmp);
+                if(modulesProperty != null) {
+                    tmp.remove(modulesProperty);
+                    tmp.removeAll(getPropertiesFor(ProjectUri.Modules.module, tmp));
+                }
+            }
+
+            //Missing groupId, use parent one Rule
+            if (getPropertyFor(ProjectUri.groupId, tmp) == null) {
+                ModelProperty parentGroupId = getPropertyFor(ProjectUri.Parent.groupId, tmp);
+                tmp.add(new ModelProperty(ProjectUri.groupId, parentGroupId.getValue()));
+            }
+
+
+            //SCM Rule
+            ModelProperty scmUrlProperty = getPropertyFor(ProjectUri.Scm.url, tmp);
+            if (scmUrl.length() == 0 && scmUrlProperty != null) {
+                scmUrl.append(scmUrlProperty.getValue());
+                for (String projectName : projectNames) {
+                    scmUrl.append("/").append(projectName);
+                }
+                int index = tmp.indexOf(scmUrlProperty);
+                tmp.remove(index);
+                tmp.add(index, new ModelProperty(ProjectUri.Scm.url, scmUrl.toString()));
+            }
+            projectNames.add(0, getPropertyFor(ProjectUri.artifactId, tmp).getValue());
+
+            modelProperties.addAll(tmp);
+
+            //Remove Parent Info
+            for(ModelProperty mp : tmp) {
+                if(mp.getUri().startsWith(ProjectUri.Parent.xUri)) {
+                    modelProperties.remove(mp);
+                }
+            }
+        }
+        return modelProperties;
+    }
+
+    private static List<ModelProperty> getPropertiesFor(String uri, List<ModelProperty> properties) {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        for (ModelProperty mp : properties) {
+            if (uri.equals(mp.getUri())) {
+                modelProperties.add(mp);
+            }
+        }
+        return modelProperties;
+    }
+
+    private static ModelProperty getPropertyFor(String uri, List<ModelProperty> properties) {
+        for (ModelProperty mp : properties) {
+            if (uri.equals(mp.getUri())) {
+                return mp;
+            }
+        }
+        return null;
+    }
+
+
+}
+
