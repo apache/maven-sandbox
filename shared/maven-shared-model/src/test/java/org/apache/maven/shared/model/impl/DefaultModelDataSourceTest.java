0516da2b47855fed2191449994d8ffd52313d686
Britton Isbell <sisbell@apache.org>
2008-07-19 20:16:10 +0000

    License headers.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@678218 13f79535-47bb-0310-9956-ffa450edef68

--- shared/maven-shared-model/src/test/java/org/apache/maven/shared/model/impl/DefaultModelDataSourceTest.java
+++ shared/maven-shared-model/src/test/java/org/apache/maven/shared/model/impl/DefaultModelDataSourceTest.java
@@ -1,5 +1,24 @@
 package org.apache.maven.shared.model.impl;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.maven.shared.model.DataSourceException;
 import org.apache.maven.shared.model.ModelContainer;
 import org.apache.maven.shared.model.ModelContainerAction;
@@ -106,18 +125,15 @@ public class DefaultModelDataSourceTest
                                            "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/artifactId",
                                            "maven-compiler-plugin" ), new ModelProperty(
             "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/groupId",
-            "org.apache.maven.plugins" ),
-
-                                                                      new ModelProperty(
-                                                                          "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin",
-                                                                          null ), new ModelProperty(
-            "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/version", "2.0.2" ),
-                                                                                  new ModelProperty(
-                                                                                      "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/artifactId",
-                                                                                      "maven-compiler-plugin" ),
-                                                                                  new ModelProperty(
-                                                                                      "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/groupId",
-                                                                                      "org.apache.maven.plugins" ) );
+            "org.apache.maven.plugins" ), new ModelProperty(
+            "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin", null ),
+                                          new ModelProperty(
+                                              "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/version",
+                                              "2.0.2" ), new ModelProperty(
+            "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/artifactId",
+            "maven-compiler-plugin" ), new ModelProperty(
+            "http://apache.org/maven/project/build/pluginManagement/plugins#collection/plugin/groupId",
+            "org.apache.maven.plugins" ) );
 
         DummyModelContainerFactory factory = new DummyModelContainerFactory();
 
