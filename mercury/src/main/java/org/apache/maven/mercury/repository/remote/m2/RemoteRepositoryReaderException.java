b83d1aedc549310305a1c31c26b1c89a631f93a7
Oleg Gusakov <ogusakov@apache.org>
2008-07-29 23:10:01 +0000

    further preparations for project split. Implemented RemoteRepositoryReaderM2 with jetty client transport. No tests yet - waiting for jetty ..
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@680874 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mercury/src/main/java/org/apache/maven/mercury/repository/remote/m2/RemoteRepositoryReaderException.java
@@ -0,0 +1,56 @@
+package org.apache.maven.mercury.repository.remote.m2;
+
+import org.apache.maven.mercury.repository.api.RepositoryException;
+
+/**
+ *
+ *
+ * @author Oleg Gusakov
+ * @version $Id$
+ *
+ */
+public class RemoteRepositoryReaderException
+extends RepositoryException
+{
+
+  /**
+   * 
+   */
+  public RemoteRepositoryReaderException()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   */
+  public RemoteRepositoryReaderException(
+      String message )
+  {
+    super( message );
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param cause
+   */
+  public RemoteRepositoryReaderException(
+      Throwable cause )
+  {
+    super( cause );
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public RemoteRepositoryReaderException(
+      String message,
+      Throwable cause )
+  {
+    super( message, cause );
+    // TODO Auto-generated constructor stub
+  }
+
+}
