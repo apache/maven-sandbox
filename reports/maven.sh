435ca181300d56b6cac14d2441345868a7007bd3
Dennis Lundberg <dennisl@apache.org>
2008-09-13 16:03:02 +0000

    o Add a system property for entityExpansionLimit go get around the SAX parser's built in max limit of 64000 entities.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@694973 13f79535-47bb-0310-9956-ffa450edef68

--- reports/maven.sh
+++ reports/maven.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export JAVA_HOME=/opt/java/sdk/current                                                                                                                          
-export JAVA_CMD=$JAVA_HOME/bin/java                                                                                                                             
-export JAVA_OPTS="-ms32m -mx256m"                                                                                                                               
-export JAVA="$JAVA_CMD $JAVA_OPTS"           
+export JAVA_HOME=/opt/java/sdk/current
+export JAVA_CMD=$JAVA_HOME/bin/java
+export JAVA_OPTS="-ms32m -mx256m"
+export JAVA="$JAVA_CMD $JAVA_OPTS"
 
-$JAVA -jar swizzle-jirareport-1.2.1-SNAPSHOT-dep.jar maven.vm > /home/j2ee-hudson/public_html/reports/maven-votes.txt
+$JAVA -jar swizzle-jirareport-1.2.1-SNAPSHOT-dep.jar maven.vm -DentityExpansionLimit=500000 > /home/j2ee-hudson/public_html/reports/maven-votes.txt
