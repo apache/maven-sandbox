4413ba5d84e638ead75569967b9ace9a056b05f5
Oleg Gusakov <ogusakov@apache.org>
2008-10-08 03:26:19 +0000

    promoting mercury out of sandbox per vote results
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@702687 13f79535-47bb-0310-9956-ffa450edef68

--- mercury/mercury-repo/mercury-repo-api/src/main/java/org/apache/maven/mercury/repository/api/RepositoryReaderFactory.java
+++
@@ -1,9 +0,0 @@
-package org.apache.maven.mercury.repository.api;
-
-import org.apache.maven.mercury.builder.api.DependencyProcessor;
-
-public interface RepositoryReaderFactory
-{
-  public RepositoryReader getReader( Repository repo, DependencyProcessor mdProcessor )
-  throws RepositoryException;
-}
