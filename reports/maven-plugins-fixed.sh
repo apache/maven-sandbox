cc45c1e1d6e3946d78cf0a50c5d42c478ad73e3a
Arnaud Heritier <aheritier@apache.org>
2008-12-17 23:37:57 +0000

    Pass output directory as parameter
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@727578 13f79535-47bb-0310-9956-ffa450edef68

--- reports/maven-plugins-fixed.sh
+++ reports/maven-plugins-fixed.sh
@@ -5,4 +5,4 @@ export JAVA_CMD=$JAVA_HOME/bin/java
 export JAVA_OPTS="-ms32m -mx256m"
 export JAVA="$JAVA_CMD $JAVA_OPTS"
 
-$JAVA -jar swizzle-jirareport-1.2.3-SNAPSHOT-dep.jar maven-plugins-fixed.vm > /home/j2ee-hudson/public_html/reports/maven-plugins-fixed.txt
+$JAVA -jar swizzle-jirareport-1.2.3-SNAPSHOT-dep.jar maven-plugins-fixed.vm > $1/maven-plugins-fixed.txt
--- reports/maven-plugins.sh
+++ reports/maven-plugins.sh
@@ -5,4 +5,4 @@ export JAVA_CMD=$JAVA_HOME/bin/java
 export JAVA_OPTS="-ms32m -mx256m"                                                                                                                               
 export JAVA="$JAVA_CMD $JAVA_OPTS"           
 
-$JAVA -jar swizzle-jirareport-1.2.3-SNAPSHOT-dep.jar maven-plugins.vm > /home/j2ee-hudson/public_html/reports/plugin-votes.txt
+$JAVA -jar swizzle-jirareport-1.2.3-SNAPSHOT-dep.jar maven-plugins.vm > $1/plugin-votes.txt
