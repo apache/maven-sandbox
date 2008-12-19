2a0818eb309506b184fdbd25e998cdadd3819e9d
Arnaud Heritier <aheritier@apache.org>
2008-12-19 06:22:04 +0000

    Fix script
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@727939 13f79535-47bb-0310-9956-ffa450edef68

--- reports/reports.sh
+++ reports/reports.sh
@@ -28,18 +28,23 @@ export CMD="$JAVA_CMD $JAVA_OPTS -jar swizzle-jirareport-1.2.3-SNAPSHOT-dep.jar"
 echo "Generating maven-votes.txt ..."
 $CMD maven.vm -DentityExpansionLimit=500000 > $OUTPUTDIR/maven-votes.txt
 echo "maven-votes.txt generated."
+
 echo "Generating maven-votes.html ..."
 $CMD maven-html.vm -DentityExpansionLimit=500000 > $OUTPUTDIR/maven-votes.html
 echo "maven-votes.html generated."
-echo "Generating maven-votes.html ..."
-$CMD maven-plugins.vm > $OUTPUTDIR/maven-votes.html
-echo "maven-votes.html generated."
+
+echo "Generating plugin-votes.txt ..."
+$CMD maven-plugins.vm > $OUTPUTDIR/plugin-votes.txt
+echo "plugin-votes.txt generated."
+
 echo "Generating plugin-votes.html ..."
 $CMD maven-plugins-html.vm > $OUTPUTDIR/plugin-votes.html
 echo "plugin-votes.html generated."
+
 echo "Generating maven-plugins-fixed.txt ..."
 $CMD maven-plugins-fixed.vm > $OUTPUTDIR/maven-plugins-fixed.txt
 echo "maven-plugins-fixed.txt generated."
+
 echo "Generating maven-plugins-fixed.html ..."
 $CMD maven-plugins-fixed-html.vm > $OUTPUTDIR/maven-plugins-fixed.html
 echo "maven-plugins-fixed.html generated."
