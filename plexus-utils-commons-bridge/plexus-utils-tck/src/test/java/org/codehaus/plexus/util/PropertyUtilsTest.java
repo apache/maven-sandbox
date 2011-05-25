634c800fc87fd09a53337fccd9458f2b2aa9aa82
Stephen Connolly <stephenc@apache.org>
2011-05-25 08:59:58 +0000

    full tck for PropertyUtils
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@1127440 13f79535-47bb-0310-9956-ffa450edef68

--- plexus-utils-commons-bridge/plexus-utils-tck/src/test/java/org/codehaus/plexus/util/PropertyUtilsTest.java
+++ plexus-utils-commons-bridge/plexus-utils-tck/src/test/java/org/codehaus/plexus/util/PropertyUtilsTest.java
@@ -19,16 +19,50 @@ package org.codehaus.plexus.util;
  * under the License.
  */
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.net.URL;
+import java.util.Properties;
 
 import static org.apache.maven.tck.TckMatchers.hasDefaultConstructor;
 import static org.apache.maven.tck.TckMatchers.isFinalClass;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class PropertyUtilsTest
 {
+    @Retention( RetentionPolicy.RUNTIME )
+    @Target( ElementType.METHOD )
+    @interface NeedsTemporaryFolder
+    {
+    }
+
+    @Rule
+    public MethodRule folderProvider = new MethodRule()
+    {
+        // keep the tests fast and only interface with the filesystem if needed
+        public Statement apply( Statement base, FrameworkMethod method, Object target )
+        {
+            if (method.getAnnotation( NeedsTemporaryFolder.class ) == null) return base;
+            folder = new TemporaryFolder();
+            return folder.apply( base, method, target );
+        }
+    };
+
+    public TemporaryFolder folder;
 
     @Test
     public void isNotUtilityClass()
@@ -36,5 +70,84 @@ public class PropertyUtilsTest
     {
         assertThat( PropertyUtils.class, allOf( hasDefaultConstructor(), not( isFinalClass() ) ) );
     }
+
+    @Test
+    public void loadNullInputStream()
+        throws Exception
+    {
+        assertThat( PropertyUtils.loadProperties( (InputStream) null ), is( new Properties() ) );
+    }
+
+    @Test
+    public void loadNullURL()
+        throws Exception
+    {
+        assertThat( PropertyUtils.loadProperties( (URL) null ), nullValue( Properties.class ) );
+    }
+
+    @Test
+    public void loadNullFile()
+        throws Exception
+    {
+        assertThat( PropertyUtils.loadProperties( (File) null ), nullValue( Properties.class ) );
+    }
+
+    @Test
+    public void loadEmptyInputStream()
+        throws Exception
+    {
+        assertThat( PropertyUtils.loadProperties( new ByteArrayInputStream( new byte[0] )), is( new Properties() ) );
+    }
+
+    @Test
+    @NeedsTemporaryFolder
+    public void loadEmptyFile()
+        throws Exception
+    {
+        assertThat( PropertyUtils.loadProperties( folder.newFile( "empty" )), is( new Properties() ) );
+    }
+
+    @Test
+    @NeedsTemporaryFolder
+    public void loadEmptyURL()
+        throws Exception
+    {
+        assertThat( PropertyUtils.loadProperties( folder.newFile( "empty" ).toURI().toURL() ), is( new Properties() ) );
+    }
+
+    @Test
+    public void loadValidInputStream()
+        throws Exception
+    {
+        Properties value = new Properties();
+        value.setProperty( "a", "b" );
+        assertThat( PropertyUtils.loadProperties( new ByteArrayInputStream( "a=b".getBytes( "ISO-8859-1" ) ) ),
+                    is( value ) );
+    }
+
+    @Test
+    @NeedsTemporaryFolder
+    public void loadValidFile()
+        throws Exception
+    {
+        File valid = folder.newFile( "valid" );
+        Properties value = new Properties();
+        value.setProperty( "a", "b" );
+        value.store( new FileOutputStream( valid ), "a test" );
+        assertThat( PropertyUtils.loadProperties( valid ), is( value ) );
+    }
+
+    @Test
+    @NeedsTemporaryFolder
+    public void loadValidURL()
+        throws Exception
+    {
+        File valid = folder.newFile( "valid" );
+        Properties value = new Properties();
+        value.setProperty( "a", "b" );
+        value.store( new FileOutputStream( valid ), "a test" );
+        assertThat( PropertyUtils.loadProperties( valid.toURI().toURL() ), is( value ) );
+    }
+
 
 }
