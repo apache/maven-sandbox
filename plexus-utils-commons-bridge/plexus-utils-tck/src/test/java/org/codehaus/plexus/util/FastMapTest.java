4da55ffa3ce4e9ce5ca7060ac3606971387fdb3f
Benson Margulies <bimargulies@apache.org>
2011-06-14 00:28:58 +0000

    Add FastMap to the picture
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@1135348 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plexus-utils-commons-bridge/plexus-utils-tck/src/test/java/org/codehaus/plexus/util/FastMapTest.java
@@ -0,0 +1,52 @@
+package org.codehaus.plexus.util;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * This is a stub test that demonstrates that something is here. It's not 
+ * worthwhile to build a complex test to prove that the current javolution
+ * is identical in behavior with the copy captured in plexus-utils.
+ *
+ */
+public class FastMapTest extends Assert
+{
+    @SuppressWarnings( "rawtypes" )
+    @Test
+    public void simpleTest() {
+        FastMap map = new FastMap();
+        map.put( "red", "green" );
+        map.put( "braised", "roasted" );
+        assertEquals( 2, map.size() );
+        Iterator it = map.entrySet().iterator();
+        Object meo1 = it.next();
+        Object meo2 = it.next();
+        Map.Entry me = (Map.Entry)meo1;
+        assertEquals( "red", me.getKey() );
+        me = (Map.Entry)meo2;
+        assertEquals( "braised", me.getKey() );
+    }
+    
+}
