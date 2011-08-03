8cdb6801357136f443dc8aa10fecb65132f941e4
Stephen Connolly <stephenc@apache.org>
2011-08-03 14:58:35 +0000

    Add some tests for reflectorHelper.... found quite a few bugs in this class
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@1153522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plexus-utils-commons-bridge/plexus-utils-tck/src/test/java/org/codehaus/plexus/util/reflection/ReflectorTestHelper.java
@@ -0,0 +1,106 @@
+package org.codehaus.plexus.util.reflection;
+
+import java.sql.Ref;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stephenc
+ * Date: 03/08/2011
+ * Time: 13:37
+ * To change this template use File | Settings | File Templates.
+ */
+class ReflectorTestHelper
+{
+    private static String PRIVATE_STATIC_STRING = "private static string";
+    static String PACKAGE_STATIC_STRING = "package static string";
+    protected static String PROTECTED_STATIC_STRING = "protected static string";
+    public static String PUBLIC_STATIC_STRING = "public static string";
+
+    private ReflectorTestHelper()
+    {
+
+    }
+
+    ReflectorTestHelper( Boolean throwSomething )
+    {
+        if ( Boolean.TRUE.equals( throwSomething ) )
+        {
+            throw new HelperException( "Something" );
+        }
+    }
+
+    protected ReflectorTestHelper( Integer throwCount )
+    {
+        if ( throwCount != null && throwCount.intValue() > 0 )
+        {
+            throw new HelperException( "Something" );
+        }
+    }
+
+    public ReflectorTestHelper( String throwMessage )
+    {
+        if ( throwMessage != null && !throwMessage.isEmpty() )
+        {
+            throw new HelperException( throwMessage );
+        }
+    }
+
+    private static ReflectorTestHelper getInstance()
+    {
+        return new ReflectorTestHelper(  );
+    }
+
+    static ReflectorTestHelper getInstance( Boolean throwSomething )
+    {
+        if ( Boolean.TRUE.equals( throwSomething ) )
+        {
+            throw new HelperException( "Something" );
+        }
+        return new ReflectorTestHelper(  );
+    }
+
+    protected static ReflectorTestHelper getInstance( Integer throwCount )
+    {
+        if ( throwCount != null && throwCount.intValue() > 0 )
+        {
+            throw new HelperException( "Something" );
+        }
+        return new ReflectorTestHelper(  );
+    }
+
+    public static ReflectorTestHelper getInstance( String throwMessage )
+    {
+        if ( throwMessage != null && !throwMessage.isEmpty() )
+        {
+            throw new HelperException( throwMessage );
+        }
+        return new ReflectorTestHelper(  );
+    }
+
+    public ReflectorTestHelper getInstance(String aString, Boolean aBoolean) {
+        return new ReflectorTestHelper(  );
+    }
+
+    public static class HelperException
+        extends RuntimeException {
+        public HelperException()
+        {
+            super();    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        public HelperException( String message )
+        {
+            super( message );    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        public HelperException( String message, Throwable cause )
+        {
+            super( message, cause );    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        public HelperException( Throwable cause )
+        {
+            super( cause );    //To change body of overridden methods use File | Settings | File Templates.
+        }
+    }
+}
