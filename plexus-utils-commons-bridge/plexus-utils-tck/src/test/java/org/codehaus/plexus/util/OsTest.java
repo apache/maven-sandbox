b57cb9288e9748dbc9d3ef9111d8fa72a418c46f
Mark Struberg <struberg@apache.org>
2011-08-11 12:23:10 +0000

    MSANDBOX-51 add Tck test for Os.java
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@1156592 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plexus-utils-commons-bridge/plexus-utils-tck/src/test/java/org/codehaus/plexus/util/OsTest.java
@@ -0,0 +1,169 @@
+package org.codehaus.plexus.util;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.tck.FixPlexusBugs;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Assert;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+
+
+/**
+ * This class will test the 'Os' class which evaluates
+ * operation system specific settings.
+ *
+ * @author <a href="mailto:struberg@yahoo.de">Mark Struberg</a>
+ */
+public class OsTest extends Assert
+{
+
+    @Rule
+    public FixPlexusBugs fixPlexusBugs = new FixPlexusBugs();
+
+    private String origOsName;
+    private String origOsArch;
+    private String origOsVersion;
+
+
+    @Before
+    public void setUp()
+            throws Exception
+    {
+        origOsName = System.getProperty( "os.name" );
+        origOsArch = System.getProperty( "os.arch" );
+        origOsVersion = System.getProperty("os.version");
+
+        // and now set some special settings ;)
+        System.setProperty( "os.name"   , "os/2" );
+        System.setProperty( "os.arch"   , "i386" );
+        System.setProperty( "os.version", "2.1.32" );
+
+        // blow away the originally loaded values
+        setStaticOsField( "OS_NAME", "os/2" );
+        setStaticOsField( "OS_ARCH", "i386" );
+        setStaticOsField( "OS_VERSION", "2.1.32" );
+    }
+
+    @After
+    public void tearDown()
+            throws Exception
+    {
+        // set the original OS settings again
+        System.setProperty( "os.name"   , origOsName );
+        System.setProperty( "os.arch"   , origOsArch );
+        System.setProperty( "os.version", origOsVersion );
+
+        // restore the originally loaded values
+        setStaticOsField( "OS_NAME", origOsName );
+        setStaticOsField( "OS_ARCH", origOsArch );
+        setStaticOsField( "OS_VERSION", origOsVersion );
+    }
+
+    private void setStaticOsField( String variableName, Object value )
+            throws NoSuchFieldException, IllegalAccessException
+    {
+        Field field = Os.class.getField( variableName );
+
+        Field modifiersField = Field.class.getDeclaredField( "modifiers" );
+        modifiersField.setAccessible( true );
+        modifiersField.setInt( field, field.getModifiers() & ~Modifier.FINAL );
+
+        field.setAccessible( true );
+        field.set( null, value );
+    }
+
+
+    @Test
+    public void testConstructor()
+            throws Exception
+    {
+        Os os  = new Os();
+        os.eval();
+
+        os = new Os( "unknown" );
+
+        assertTrue(Os.isName(Os.FAMILY_OS2));
+
+        assertFalse( Os.isName( Os.FAMILY_DOS ) );
+        assertFalse( Os.isName( Os.FAMILY_MAC ) );
+        assertFalse( Os.isName( Os.FAMILY_NETWARE ) );
+        assertFalse( Os.isName( Os.FAMILY_OPENVMS ) );
+        assertFalse( Os.isName( Os.FAMILY_OS400 ) );
+        assertFalse( Os.isName( Os.FAMILY_TANDEM ) );
+        assertFalse( Os.isName( Os.FAMILY_UNIX ) );
+        assertFalse( Os.isName( Os.FAMILY_WIN9X ) );
+        assertFalse( Os.isName( Os.FAMILY_WINDOWS ) );
+        assertFalse( Os.isName(Os.FAMILY_ZOS ) );
+    }
+
+    @Test
+    public void testFamilyNames()
+    {
+        assertEquals( Os.FAMILY_DOS, "dos" );
+        assertEquals( Os.FAMILY_MAC, "mac" );
+        assertEquals( Os.FAMILY_NETWARE, "netware" );
+        assertEquals( Os.FAMILY_OPENVMS, "openvms" );
+        assertEquals( Os.FAMILY_OS2, "os/2" );
+        assertEquals( Os.FAMILY_OS400, "os/400" );
+        assertEquals( Os.FAMILY_TANDEM, "tandem" );
+        assertEquals( Os.FAMILY_UNIX, "unix" );
+        assertEquals( Os.FAMILY_WIN9X, "win9x" );
+        assertEquals( Os.FAMILY_WINDOWS, "windows" );
+        assertEquals( Os.FAMILY_ZOS, "z/os" );
+    }
+
+    @Test
+    public void testGetValidFamilies()
+    {
+        Set<String> osFamilies = Os.getValidFamilies();
+
+        assertThat( "OsFamilies Set size"
+                  , osFamilies.size()
+                  , is(11) );
+        
+        assert( osFamilies.contains( Os.FAMILY_DOS ) );
+        assert( osFamilies.contains( Os.FAMILY_MAC ) );
+        assert( osFamilies.contains( Os.FAMILY_NETWARE ) );
+        assert( osFamilies.contains( Os.FAMILY_OPENVMS ) );
+        assert( osFamilies.contains( Os.FAMILY_OS2 ) );
+        assert( osFamilies.contains( Os.FAMILY_OS400 ) );
+        assert( osFamilies.contains( Os.FAMILY_TANDEM ) );
+        assert( osFamilies.contains( Os.FAMILY_UNIX ) );
+        assert( osFamilies.contains( Os.FAMILY_WIN9X ) );
+        assert( osFamilies.contains( Os.FAMILY_WINDOWS ) );
+        assert( osFamilies.contains( Os.FAMILY_ZOS ) );
+        
+    }
+
+
+    @Test
+    public void testIsOs()
+    {
+        Os myOs= new Os( "unix");
+    }
+}
